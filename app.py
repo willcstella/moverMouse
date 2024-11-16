04a74db2553280413e08a350e9fc0fef62cebe92
William Corrêa de Oliveira Stella <willcstella@gmail.com>
2024-11-16 17:24:19 -0300

    Desafio Jogo

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 import pyautogui
+import time
+# Importa o mouseInfo do biblioteca mouse info no cmd
+# python
+#from mouseinfo import mouseInfo
+#mouseInfo()
 
 # https://www.crazygames.com/game/doge-miner-2
 
-# Posição de algo
-# Fazer algo com a posição
+# Posição de algo - use o mouseInfo()
+#pyautogui.moveTo(1249,53, duration=2)
+# Fazer algo com a posição de onde estava
+#pyautogui.move(45,0, duration=2) # Move a partide  um ponto colo no exemplo + 45px em cima de 1249px
+#pyautogui.move(0,-50, duration=2)
+#pyautogui.click()
+
+
+# códido para clicar no jogo
+
+pyautogui.moveTo(913,424, duration=2)
+pyautogui.click(clicks=9000, duration=3)
